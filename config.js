55aa752eb4e19f7b3a1df347f5a59e58d354afd0
Aryan Khatri <mr.aryankhatri@gmail.com>
2026-01-24 19:12:15 +0530

    fixed frontend deployment issues

--- config.js
+++ config.js
@@ -6,8 +6,9 @@ window.API_BASE_URL = "https://vybey-backened.onrender.com";
   const originalFetch = window.fetch;
 
   window.fetch = function (url, options) {
-    if (typeof url === "string" && url.includes("localhost:5000")) {
-      url = url.replace("http://localhost:5000", window.API_BASE_URL);
+    if (typeof url === "string") {
+      url = url.replace("https://vybey-backened.onrender.com", window.API_BASE_URL);
+      url = url.replace("https://localhost:5000", window.API_BASE_URL);
     }
     return originalFetch(url, options);
   };
